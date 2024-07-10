67d33d4c97316514c4cd13412c613b6a5ac6d5da
Gautami Mamidala <gautamisaisatya@gmail.com>
2024-07-10 07:27:38 +0000

    added few files

---
+++ etl/etl/assets.py
@@ -0,0 +1,18 @@
+@asset(deps=[topstory_ids])
+def topstories() -> None:
+    with open("data/topstory_ids.json", "r") as f:
+        topstory_ids = json.load(f)
+
+    results = []
+    for item_id in topstory_ids:
+        item = requests.get(
+            f"https://hacker-news.firebaseio.com/v0/item/{item_id}.json"
+        ).json()
+        results.append(item)
+
+        if len(results) % 20 == 0:
+            print(f"Got {len(results)} items so far.")
+
+    df = pd.DataFrame(results)
+    df.to_csv("data/topstories.csv")
+
